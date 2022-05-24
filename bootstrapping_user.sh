122155e535b278e435e3584a36ba462a8db3bc80
Erick Saenz <ericklife@hotmail.com>
2022-05-24 04:58:52 +0000

    Adding new permission

--- bootstrapping_user.sh
+++ bootstrapping_user.sh
@@ -6,3 +6,5 @@ gcloud iam service-accounts create sa-iac-bootstrap --display-name="IaC Bootstra
 gcloud projects add-iam-policy-binding $PROJECT_ID --member="serviceAccount:sa-iac-bootstrap@$PROJECT_ID.iam.gserviceaccount.com" --role=roles/resourcemanager.projectIamAdmin
 
 gcloud projects add-iam-policy-binding $PROJECT_ID --member="serviceAccount:sa-iac-bootstrap@$PROJECT_ID.iam.gserviceaccount.com" --role=roles/storage.objectAdmin
+
+gcloud projects add-iam-policy-binding $PROJECT_ID --member="serviceAccount:sa-iac-bootstrap@$PROJECT_ID.iam.gserviceaccount.com" --role=roles/logging.logWriter
